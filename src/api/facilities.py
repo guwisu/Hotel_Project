9f7dcef68dfc9b97544a1ee36e7d00ed785be6f3
guwisu <kolyabelykov@mail.ru>
2025-06-30 16:49:29 +0400

    add celery and tasks

--- src/api/facilities.py
+++ src/api/facilities.py
@@ -1,27 +1,19 @@
-import json
-
 from fastapi import APIRouter, Body
+from fastapi_cache.decorator import cache
+
 from src.api.dependencies import DBDep
 from src.init import redis_manager
 from src.schemas.facilities import FacilityAdd
+from src.tasks.tasks import test_task
 
 router = APIRouter(prefix="/facilities", tags=["Удобства"])
 
 
 @router.get("/")
+@cache(expire=10)
 async def get_facilities(db: DBDep):
-    facilities_from_cache = await redis_manager.get("facilities")
-    if not facilities_from_cache:
-        print("Иду в бд")
-        facilities = await db.facilities.get_all()
-        facilities_schemas: list[dict] = [f.model_dump() for f in facilities]
-        facilities_json = json.dumps(facilities_schemas)
-        await redis_manager.set("facilities", facilities_json, 10)
-
-        return facilities
-    else:
-        facilities_dicts = json.loads(facilities_from_cache)
-        return facilities_dicts
+    print("Иду в бд")
+    return await db.facilities.get_all()
 
 @router.post("/")
 async def add_facility(
@@ -30,4 +22,7 @@ async def add_facility(
 ):
     facility = await db.facilities.add(data=facility_data)
     await db.commit()
+
+    test_task.delay()
+
     return {"status": "OK", "data": facility}
