a0a9d7af0b6e6a73589211a5efe89473d8d1ef08
guwisu <kolyabelykov@mail.ru>
2025-06-30 13:06:28 +0400

    add Redis in facilities

--- src/api/facilities.py
+++ src/api/facilities.py
@@ -1,6 +1,8 @@
-from fastapi import APIRouter, Body
+import json
 
+from fastapi import APIRouter, Body
 from src.api.dependencies import DBDep
+from src.init import redis_manager
 from src.schemas.facilities import FacilityAdd
 
 router = APIRouter(prefix="/facilities", tags=["Удобства"])
@@ -8,8 +10,18 @@ router = APIRouter(prefix="/facilities", tags=["Удобства"])
 
 @router.get("/")
 async def get_facilities(db: DBDep):
-    return await db.facilities.get_all()
+    facilities_from_cache = await redis_manager.get("facilities")
+    if not facilities_from_cache:
+        print("Иду в бд")
+        facilities = await db.facilities.get_all()
+        facilities_schemas: list[dict] = [f.model_dump() for f in facilities]
+        facilities_json = json.dumps(facilities_schemas)
+        await redis_manager.set("facilities", facilities_json, 10)
 
+        return facilities
+    else:
+        facilities_dicts = json.loads(facilities_from_cache)
+        return facilities_dicts
 
 @router.post("/")
 async def add_facility(
--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 from fastapi.openapi.docs import get_swagger_ui_html
 import uvicorn
@@ -5,15 +7,26 @@ import uvicorn
 import sys
 from pathlib import Path
 
+
+
 sys.path.append(str(Path(__file__).parent.parent))
 
+from src.init import redis_manager
 from src.api.hotels import router as router_hotels
 from src.api.auth import router as router_auth
 from src.api.rooms import router as router_rooms
 from src.api.bookings import router as router_bookings
 from src.api.facilities import router as router_facilities
 
-app = FastAPI(docs_url=None)
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    #При старте приложения
+    await redis_manager.connect()
+    yield
+    await redis_manager.close()
+    #При выключении/перезагрузке приложения
+
+app = FastAPI(docs_url=None, lifespan=lifespan)
 
 app.include_router(router_auth)
 app.include_router(router_hotels)
