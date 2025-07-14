e2941bfbdcaf5cf62870eaac687096499ecf85f8
guwisu <kolyabelykov@mail.ru>
2025-07-14 15:00:58 +0400

    style: add redirect docs and README.md

--- src/main.py
+++ src/main.py
@@ -9,6 +9,7 @@ from fastapi.openapi.docs import get_swagger_ui_html
 from fastapi_cache import FastAPICache
 from fastapi_cache.backends.redis import RedisBackend
 import uvicorn
+from starlette.middleware.cors import CORSMiddleware
 
 sys.path.append(str(Path(__file__).parent.parent))
 
@@ -22,6 +23,7 @@ from src.api.rooms import router as router_rooms  # noqa: E402
 from src.api.bookings import router as router_bookings  # noqa: E402
 from src.api.facilities import router as router_facilities  # noqa: E402
 from src.api.images import router as router_images  # noqa: E402
+from src.api.root import router as router_root  # noqa: E402
 
 
 @asynccontextmanager
@@ -44,6 +46,7 @@ app.include_router(router_rooms)
 app.include_router(router_facilities)
 app.include_router(router_bookings)
 app.include_router(router_images)
+app.include_router(router_root)
 
 
 @app.get("/docs", include_in_schema=False)
@@ -55,6 +58,9 @@ async def custom_swagger_ui_html():
         swagger_js_url="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js",
         swagger_css_url="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css",
     )
+
+
+app.add_middleware(CORSMiddleware, allow_origins=["*"])
 
 
 if __name__ == "__main__":
